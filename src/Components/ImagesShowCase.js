f385120f797f25c52878ee1587fd989aeb0e7627
Himanshu Verma <himanshuverma544@gmail.com>
2023-02-28 12:30:45 +0530

    add & update: - adding up the images in the image container - using progressive-image and lazy-load approach - setting the loading icon for the images

---
+++ src/Components/ImagesShowCase.js
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import { Col } from "reactstrap";
+import ProgressiveImage from "react-progressive-graceful-image";
+import { Hearts } from "react-loading-icons";
+import randomColor from "randomcolor";
+
+
+const ImagesShowCase = ({ imagesToLoad }) => {
+  
+  return (
+    <>
+      {imagesToLoad.map(imageToLoad => (
+        <Col className="py-3" key={imageToLoad.id} md={3}>
+          <ProgressiveImage src={imageToLoad.regular} placeholder={imageToLoad.thumb}>
+            {(src, loading) => (
+              <div className="image-container position-relative" style={{backgroundColor : randomColor()}}>
+                <Hearts className="hearts-loading-icon ms-3 position-absolute top-0 start-0" style={{ display : loading ? "block" : "none" }} stroke="#000"/>
+                <img className="image" src={src} alt={imageToLoad.alt}/>
+              </div>
+            )}
+          </ProgressiveImage>
+        </Col>
+      ))}    
+    </>  
+  );
+};
+
+export default memo(ImagesShowCase);
