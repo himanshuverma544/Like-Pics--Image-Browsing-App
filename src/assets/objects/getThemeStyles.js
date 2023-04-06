0bbfc806fcb0c4c80517a21ddd9119f2981ffacf
Himanshu Verma <himanshuverma544@gmail.com>
2023-04-06 20:17:09 +0530

    add: created object for light and dark theme styling

---
+++ src/assets/objects/getThemeStyles.js
@@ -0,0 +1,29 @@
+const getThemeStyles = {
+  
+  light: {
+    body: {
+      backgroundColor: "#FFF"
+    },
+    heading: {
+      textColor: "#000",
+    },
+    img: {
+      name: "dark-theme-icon.png",
+      alt: "dark theme icon"
+    }
+  },
+  dark: {
+    body: {
+      backgroundColor: "#252525"
+    },
+    heading: {
+      textColor: "#FFF",
+    },
+    img: {
+      name: "light-theme-icon.png",
+      alt: "light theme icon"
+    }
+  }
+}
+
+export default getThemeStyles;
