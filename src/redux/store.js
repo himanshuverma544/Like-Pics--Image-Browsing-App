f3be757c98fdec37857ddac92523f7c94f6c1912
Himanshu Verma <himanshuverma544@gmail.com>
2023-04-15 19:07:23 +0530

    update: removed globalData reducer from the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import imagesReducer from "./imagesSlice";
-import globalDataReducer from "./globalDataSlice";
 
 const store = configureStore({
   reducer: {
-    imagesReducer,
-    globalDataReducer
-  }
+    imagesReducer  }
 });
 
 export default store;
