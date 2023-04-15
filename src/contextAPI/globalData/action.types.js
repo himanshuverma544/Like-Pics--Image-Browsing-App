65d9d3b44e33198d682bdf21635fac77cf709d73
Himanshu Verma <himanshuverma544@gmail.com>
2023-04-15 19:08:07 +0530

    add: created action types for the global data

---
+++ src/contextAPI/globalData/action.types.js
@@ -0,0 +1,4 @@
+export const STORE_STATES = "STORE_STATES";
+export const STORE_NODES = "STORE_NODES";
+export const STORE_VARIABLES = "STORE_VARIABLES";
+export const STORE_FUNCTIONS = "STORE_FUNCTIONS";
