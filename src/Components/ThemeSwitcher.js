00fd686fa93d9080c405a33d8ee261e27e6f4d5e
Himanshu Verma <himanshuverma544@gmail.com>
2023-04-27 10:57:13 +0530

    update: - used the context and store the data - manages the component accordingly to context - manages the button panel theme icon

--- src/Components/ThemeSwitcher.js
+++ src/Components/ThemeSwitcher.js
@@ -1,7 +1,7 @@
-import { useState, useEffect, useRef, useContext, memo } from "react";
+import { useState, useEffect, useContext, memo } from "react";
 
-import globalDataContext from "../contextAPI/globalData/context";
-import { storeInGlobalData } from "../contextAPI/globalData/action.creators";
+import { tsContext } from "../contextAPI/globalData/context";
+import { storeData } from "../contextAPI/globalData/action.creators";
 
 import { Row, Col } from "reactstrap";
 import { MdOutlineLightMode, MdOutlineNightlight } from "react-icons/md";
@@ -16,75 +16,88 @@ const ThemeSwitcher = () => {
   });
   const currentTheme = getThemeStyles[theme];
 
-  const themeNode = useRef(null);
-
-  const { dispatch } = useContext(globalDataContext);
-
-
-  // useEffect(() => {
-  //   function storeGlobalData() {
-  //     dispatch(storeInGlobalData("ButtonPanel", "states", {setTheme}));
-  //     dispatch(storeInGlobalData("ButtonPanel", "states", {theme}));
-  //   }
-  //   storeGlobalData();
-  // }, [dispatch]);
+  const { tsData: { triggered }, tsDispatch: dispatch } = useContext(tsContext);
 
   useEffect(() => {
-    function setLocalTheme() {
-      localStorage.setItem("localTheme", theme);
-    }
-    setLocalTheme();
-  }, [theme]);
+      function storeGlobalData() {
+        dispatch(storeData({theme, setTheme}, "states"));
+      }
+      storeGlobalData();
+  }, [theme, dispatch]);
 
 
   useEffect(() => {
-
-    function switchTheme() {
-
-      document.body.style.backgroundColor = currentTheme.body.backgroundColor;
-
-      ["h1", "h6"].forEach(headingTags => {
-        document.querySelectorAll(headingTags).forEach(element => 
-          element.style.color = currentTheme.heading.textColor
-        );
-      });
-
-      document.querySelector(".theme-icon").style.fill = currentTheme.themeIcon.fill;
-      
-      const btnsPanelOpenIcon = document.querySelector(".btns-panel-container .open-icon");
-      btnsPanelOpenIcon.style.backgroundColor = currentTheme.btnsPanelOpenIcon.backgroundColor;
-      btnsPanelOpenIcon.style.stroke = currentTheme.btnsPanelOpenIcon.stroke;
-
-      document.querySelectorAll(".panel-btn").forEach(btn => {
-
-        btn.style.backgroundColor = currentTheme.btnsPanelBtns.default.backgroundColor;
-           
-        btn.addEventListener("mouseover", () => 
-          btn.style.backgroundColor = currentTheme.btnsPanelBtns.onHover.backgroundColor
-        );
-        
-        btn.addEventListener("mouseout", () => 
-          btn.style.backgroundColor = currentTheme.btnsPanelBtns.default.backgroundColor
-        );
-        
-      });
-
-      document.querySelectorAll(".panel-icon").forEach(icon =>
-        icon.style.fill = currentTheme.btnsPanelBtnsIcons.fill
-      );
+    if (triggered) {
+      function setLocalTheme() {
+        localStorage.setItem("localTheme", theme);
+      }
+      setLocalTheme();
     }
-    switchTheme();
+  }, [triggered, theme]);
 
-  }, [currentTheme]);
+
+  useEffect(() => {
+
+    if (triggered) {
+
+      function switchTheme() {
+
+        document.body.style.backgroundColor = currentTheme.body.backgroundColor;
+
+        ["h1", "h6"].forEach(headingTags => {
+          document.querySelectorAll(headingTags).forEach(element => 
+            element.style.color = currentTheme.heading.textColor
+          );
+        });
+
+        document.querySelector(".theme-icon").style.fill = currentTheme.themeIcon.fill;
+        
+        const btnsPanelOpenIcon = document.querySelector(".btns-panel-container .open-icon");
+        btnsPanelOpenIcon.style.backgroundColor = currentTheme.btnsPanelOpenIcon.backgroundColor;
+        btnsPanelOpenIcon.style.stroke = currentTheme.btnsPanelOpenIcon.stroke;
+
+        document.querySelectorAll(".panel-btn").forEach(btn => {
+
+          btn.style.backgroundColor = currentTheme.btnsPanelBtns.default.backgroundColor;
+            
+          btn.addEventListener("mouseover", () => 
+            btn.style.backgroundColor = currentTheme.btnsPanelBtns.onHover.backgroundColor
+          );
+          
+          btn.addEventListener("mouseout", () => 
+            btn.style.backgroundColor = currentTheme.btnsPanelBtns.default.backgroundColor
+          );
+          
+        });
+
+        document.querySelectorAll(".panel-icon").forEach(icon =>
+          icon.style.fill = currentTheme.btnsPanelBtnsIcons.fill
+        );
+        
+        document.querySelectorAll(currentTheme.themeIcon.hideIconClassName).forEach(icon =>
+          icon.style.display = "none"
+        );
+
+        document.querySelectorAll(currentTheme.themeIcon.showIconClassName).forEach(icon =>
+          icon.style.display = "block"
+        ); 
+      }
+      switchTheme();
+    }
+  }, [triggered, currentTheme]);
+
 
   return (
+    triggered ? (
     <Row>
-      <Col md={12}>
-        <div rel={themeNode} onClick={() => setTheme(prevTheme => prevTheme === "dark" ? "light" : "dark")}>
-          {theme === "dark" ? <MdOutlineLightMode className="theme-icon"/> : <MdOutlineNightlight className="theme-icon dark-theme-icon"/>} 
+      <Col>
+        <div onClick={() => setTheme(prevTheme => prevTheme === "dark" ? "light" : "dark")}>
+          <MdOutlineLightMode className="theme-icon light-theme-icon"/>
+          <MdOutlineNightlight className="theme-icon dark-theme-icon"/> 
         </div> 
       </Col>
     </Row>
+    ) : null
   );
 };
 
