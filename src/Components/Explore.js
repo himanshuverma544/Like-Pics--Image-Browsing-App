281bd23cb20872c0cc659cee8812bf037506f469
Himanshu Verma <himanshuverma544@gmail.com>
2023-07-10 01:12:52 +0530

    upd: - added loader for images - imported icons for sign in and saved images - uncommented AutoSuggestions and typewriter

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -1,7 +1,8 @@
 // frontend libraries
 import { Container, Row, Col, Button, Form, Input, InputGroup } from "reactstrap";
 import { BsSearch } from "react-icons/bs";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MdOutlineBookmarkAdd, MdOutlineBookmarkAdded } from "react-icons/md";
 
 // react hooks
 import { useState, useEffect, useContext, useRef, useCallback, useMemo } from "react";
@@ -106,89 +107,98 @@ const Explore = () => {
 
 
   return (
-    <Container className="py-3">
-    
-      <ThemeSwitcher/>
+    <>
+      <Container fluid className="py-3">
+        <Row>
+          <Col class="d-flex justify-content-end">
+            {/* <MdOutlineBookmarkAdded class="saved-images-icon me-4"/> */}
+            <ThemeSwitcher/>
+            {/* <AiOutlineUser class="user-icon me-2"/> */}
+          </Col>
+        </Row>
+      </Container>
 
-      <Row>
-        <Col md={12}>
-          <div className="hero-sec d-flex justify-content-center align-items-center position-relative">
-            <h1 className="me-3">Like Pics</h1>
-            <img className="app-icon" src={getImage("like-icon.png")} alt="Like Icon"/>
-          </div>
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="hero-sec d-flex justify-content-center align-items-center position-relative">
+              <h1 className="me-3">Like Pics</h1>
+              <img className="app-icon" src={getImage("like-icon.png")} alt="Like Icon"/>
+            </div>
+          </Col>
+        </Row>
 
-      <Row>
-        <Col md={12}>
-          <Form
-            className="position-relative"
-            onSubmit={event => {
-              event.preventDefault();
-              handleSearchImages();
-          }}>
-            <InputGroup>
-              <Input
-                type="text"
-                id="search-field"
-                className="ps-3"
-                innerRef={searchValueNode}
-                onChange={event => setSearchQuery(event.target.value.trim())}
-                autoComplete="off"
-                // placeholder={
-                //   typewriter({
-                //     leftStaticStr: "Search for ", 
-                //     words: popularImageSearchWords, 
-                //     rightStaticStr: " from the library of over 3.48 million plus photos",
-                //   })
-                // }
-                autoFocus
+        <Row>
+          <Col md={12}>
+            <Form
+              className="position-relative"
+              onSubmit={event => {
+                event.preventDefault();
+                handleSearchImages();
+            }}>
+              <InputGroup>
+                <Input
+                  type="text"
+                  id="search-field"
+                  className="ps-3"
+                  innerRef={searchValueNode}
+                  onChange={event => setSearchQuery(event.target.value.trim())}
+                  autoComplete="off"
+                  placeholder={
+                    typewriter({
+                      leftStaticStr: "Search for ", 
+                      words: popularImageSearchWords, 
+                      rightStaticStr: " from the library of over 3.48 million plus photos",
+                    })
+                  }
+                  autoFocus
+                />
+                <Button
+                  className="search-btn"
+                  color="danger"
+                >
+                  <BsSearch/>
+                </Button>
+              </InputGroup>
+              <AutoSuggestions
+                states={{searchQuery, setSearchQuery}}
+                nodes={{searchValueNode}}
+                variables={{limit: 5}}
+                functions={{handleSearchImages}}
               />
-              <Button
-                className="search-btn"
-                color="danger"
-              >
-                <BsSearch/>
-              </Button>
-            </InputGroup>
-            {/* <AutoSuggestions
-              states={{searchQuery, setSearchQuery}}
-              nodes={{searchValueNode}}
-              variables={{limit: 5}}
-              functions={{handleSearchImages}}
-            /> */}
-          </Form>
-        </Col>
-      </Row>
-      
-      { (isSuccess && imagesData.length) && 
-        <Row className="images-showcase-row">
-          <ImagesGridView 
-            ref={ref} 
-            imagesData={imagesData} 
-          />
+            </Form>
+          </Col>
         </Row>
-      }
-
-      { isFetchingNextPage &&
-        <Row className="images-loading-icon-row">
-          <AiOutlineLoading3Quarters 
-            className="images-loading-icon" 
-            style={{ color: states.theme === DARK_THEME ? "#FFF" : "#000" }}
-          />
-        </Row>
-      }
-    
-      <Row className="btns-panel-row">
-        <Col>
-          <div className="btns-panel-container d-flex justify-content-center">
-            <ButtonsPanel
-              nodes={{ searchValueNode }}
+        
+        { (isSuccess && imagesData.length) && 
+          <Row className="images-showcase-row">
+            <ImagesGridView 
+              ref={ref} 
+              imagesData={imagesData} 
             />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Row>
+        }
+
+        { isFetchingNextPage &&
+          <Row className="images-loading-icon-row">
+            <AiOutlineLoading3Quarters 
+              className="images-loading-icon" 
+              style={{ color: states.theme === DARK_THEME ? "#FFF" : "#000" }}
+            />
+          </Row>
+        }
+      
+        <Row className="btns-panel-row">
+          <Col>
+            <div className="btns-panel-container d-flex justify-content-center">
+              <ButtonsPanel
+                nodes={{ searchValueNode }}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
