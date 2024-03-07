461e777b03f89587c4b3e13a29b14e6862336890
Himanshu Verma <himanshuverma544@gmail.com>
2024-03-08 00:15:30 +0530

    add: created modal component to view image

---
+++ src/Components/Modals/ViewImageModal.js
@@ -0,0 +1,24 @@
+import { createPortal } from "react-dom";
+
+import { memo } from "react";
+
+import ProgressiveImage from "react-progressive-graceful-image";
+
+
+const ViewImageModal = ({ imageIndexes: {i, j}, imagesData, viewImageModalNode, closeViewImageModal }) => {
+
+  const image = imagesData[i].results[j];
+
+  return createPortal(
+    <div className="the-view-image-modal" onClick={event => closeViewImageModal(event)}>
+      <ProgressiveImage src={image.urls.full} placeholder={image.urls.regular}>
+        {(src, loading) => (
+          <img ref={viewImageModalNode} src={src} alt={image.alt_description}/>
+        )}
+      </ProgressiveImage>
+    </div>,
+    document.getElementById("view-image-modal")
+  );
+}
+
+export default memo(ViewImageModal);
