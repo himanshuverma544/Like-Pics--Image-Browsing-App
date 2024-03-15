1300b4a8cc071b78464b7b420caa7e860f4f5b0a
Himanshu Verma <himanshuverma544@gmail.com>
2024-03-15 11:39:37 +0530

    upd: wrapped the app with required components like, - context provider - provider - react query

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import ContextProvider from "./context-API/ContextProvider";
+
+import "./configurations/firebase";
+
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <ContextProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App/>
+      </QueryClientProvider>
+    </Provider>
+  </ContextProvider>
 );
