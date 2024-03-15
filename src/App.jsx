9efe167ad839da342371e13aa95b31cda3e19e77
Himanshu Verma <himanshuverma544@gmail.com>
2024-03-15 11:38:20 +0530

    upd: added the routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,28 @@ import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import "./configurations/firebase";
+import Layout from './pages/Layout';
+import Explore from "./pages/Explore";
+import Saved from './pages/Saved';
+import Page404 from './pages/Page404';
 
-import { tsContext as TSContext } from "./context";
-import dataReducer from "./reducers/globalData";
-import { useReducer } from "react";
-import { QueryClient, QueryClientProvider } from 'react-query';
-
-import Explore from "./components/Explore";
-
-
-const initialState = {
-
-  states: null,
-  nodes: null,
-  objects: null,
-  arrays: null,
-  refVars: null,
-  vars: null,
-  triggered: false
-};
-
-const queryClient = new QueryClient();
+import { EXPLORE, SAVED, PAGE404 } from "./utils/constants";
 
 
 const App = () => {
 
-  const [tsData, tsDispatch] = useReducer(dataReducer, initialState);
-
   return (
-    <Provider store={store}>
-      <TSContext.Provider value={{ tsData, tsDispatch }}>
-        <QueryClientProvider client={queryClient}>
-          <Explore/>
-        </QueryClientProvider>
-      </TSContext.Provider>
-    </Provider>
+    <HashRouter basename=''>
+      <Routes>
+        <Route path={EXPLORE.pathname} element={<Layout/>}>
+          <Route index element={<Explore/>}/>
+          <Route path={SAVED.pathname} element={<Saved/>}/>
+          <Route path={PAGE404.pathname} element={<Page404/>}/>
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 };
 
